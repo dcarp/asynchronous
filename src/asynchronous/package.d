d5a131c01e712d993bd5022371ab96a6fcd47cd2
Dragos Carp <dragoscarp@gmail.com>
2015-05-08 17:22:19 +0200

    Add streams

--- src/asynchronous/package.d
+++ src/asynchronous/package.d
@@ -2,6 +2,8 @@ module asynchronous;
 
 public import asynchronous.events;
 public import asynchronous.futures;
+public import asynchronous.protocols;
+public import asynchronous.streams;
 public import asynchronous.tasks;
 public import asynchronous.transports;
 public import asynchronous.types;

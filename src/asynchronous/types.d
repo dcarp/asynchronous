85252823fe1206809e2e9a8c8733c4fac4dfe121
Dragos Carp <dragoscarp@gmail.com>
2016-03-17 15:50:42 +0100

    Bugfix in ResourcePool (Array!bool is buggy)

--- src/asynchronous/types.d
+++ src/asynchronous/types.d
@@ -72,8 +72,7 @@ class NotImplementedException : Exception
 class ResourcePool(T, TArgs...)
 if (is(T == class))
 {
-    private size_t inUseCount;
-    private Array!bool inUseFlags;
+    private Array!ubyte inUseFlags;
     private Array!T resources;
 
     private TArgs tArgs;
@@ -100,15 +99,14 @@ if (is(T == class))
         }
         else
         {
-            inUseFlags.insertBack(true);
             result = new T(tArgs);
+            inUseFlags.insertBack(true);
             resources.insertBack(result);
         }
 
         static if (hasMember!(T, "reset"))
             result.reset(tResetArgs);
 
-        ++inUseCount;
         return result;
     }
 
@@ -117,24 +115,90 @@ if (is(T == class))
         auto count = resources[].countUntil!(a => a is t);
 
         enforce(count >= 0, "Cannot release non-acquired resource");
+        enforce(inUseFlags[count], "Resource is already released");
+
+        inUseFlags[count] = false;
 
         static if (hasMember!(T, "reset"))
             resources[count].reset;
-
-        inUseFlags[count] = false;
-        --inUseCount;
     }
 
-    bool empty()
+    @property size_t length() const
     {
-        return inUseCount == 0;
+        return inUseFlags[].count!(a => !!a);
+    }
+
+    @property size_t capacity() const
+    {
+        return inUseFlags.length;
     }
 
     override string toString() const
     {
         import std.format : format;
 
-        return "%s(length: %s, capacity: %s)".format(typeid(this),
-            inUseCount, inUseFlags.length);
+        return "%s(length: %s, capacity: %s)".format(typeid(this), length,
+            capacity);
     }
+}
+
+unittest
+{
+    import std.algorithm : each, equal, map;
+    import std.range : array, drop, iota, take;
+
+    static class Foo
+    {
+        int bar;
+        int baz;
+
+        this(int baz)
+        {
+            this.baz = baz;
+        }
+
+        void reset(int bar = 0)
+        {
+            this.bar = bar;
+        }
+    }
+
+    auto fooPool = new ResourcePool!(Foo, int)(10);
+
+    auto foo = fooPool.acquire(100);
+    assert(foo.bar == 100);
+    assert(foo.baz == 10);
+    fooPool.release(foo);
+    assert(foo.bar == 0);
+    assert(foo.baz == 10);
+
+    auto foos1 = iota(100).map!(a => fooPool.acquire(a)).array;
+    assert(fooPool.length == 100);
+    assert(fooPool.capacity == 100);
+    assert(foos1.map!(a => a.bar).equal(iota(100)));
+
+    foos1.take(20).each!(a => fooPool.release(a));
+    foos1 = foos1.drop(20);
+    assert(fooPool.length == 80);
+    assert(fooPool.capacity == 100);
+
+    auto foos2 = iota(10).map!(a => fooPool.acquire(a)).array;
+    assert(fooPool.length == 90);
+    assert(fooPool.capacity == 100);
+
+    auto foos3 = iota(20).map!(a => fooPool.acquire(a)).array;
+    assert(fooPool.length == 110);
+    assert(fooPool.capacity == 110);
+
+    foos1.each!(a => fooPool.release(a));
+    assert(fooPool.length == 30);
+    assert(fooPool.capacity == 110);
+
+    foos2.each!(a => fooPool.release(a));
+    assert(fooPool.length == 20);
+    assert(fooPool.capacity == 110);
+
+    foos3.each!(a => fooPool.release(a));
+    assert(fooPool.length == 0);
+    assert(fooPool.capacity == 110);
 }
